0b1d969ce4230373029e0ed45b014edfdc4303b4
Yang Tang <yangtang2009@u.northwestern.edu>
2016-04-30 13:17:51 -0500

    add progress bar

--- app/example/scripts/VideoController.js
+++ app/example/scripts/VideoController.js
@@ -1,7 +1,9 @@
 angular
   .module('example')
   .controller('VideoController', function($scope, supersonic) {
-  	var video = document.getElementById("myvideo")
+  	var video = document.getElementById("myvideo");
+    var seekBar = document.getElementById("seek-bar");
+
 	if (video.hasAttribute("controls")) {
 	    video.removeAttribute("controls")   
     }
@@ -16,4 +18,41 @@ angular
     	}
     }
     $scope.test = "hi"
+
+
+    // Event listener for the seek bar
+    seekBar.addEventListener("change", function() {
+      // Calculate the new time
+      var time = video.duration * (seekBar.value / 100);
+
+      // Update the video time
+      video.currentTime = time;
+    });
+
+    // Update the seek bar as the video plays
+    video.addEventListener("timeupdate", function() {
+      // Calculate the slider value
+      var value = (100 / video.duration) * video.currentTime;
+
+      // Update the slider value
+      seekBar.value = value;
+    });
+
+    // Pause the video when the slider handle is being dragged
+    seekBar.addEventListener("mousedown", function() {
+      video.pause();
+    });
+
+    // Play the video when the slider handle is dropped
+    seekBar.addEventListener("mouseup", function() {
+      video.play();
+    });
+
+
+
+
+
+
   });
+
+
